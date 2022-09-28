a2df470343004f299f56c60d764a1b973b08db40
hanjihye14 <113482808+hanjihye14@users.noreply.github.com>
2022-09-28 11:38:14 +0900

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
         })
 
         log.Println("start server")
-        server := &http.Server{Addr: ":8080"}
+        server := &http.Server{Addr: ":8070"}
         if err := server.ListenAndServe(); err != nil {
                 log.Println(err)
         }
